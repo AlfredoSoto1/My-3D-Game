4af687b2a2183f6883b341645049f1d6742b710e
AlfredoSoto1 <108200951+AlfredoSoto1@users.noreply.github.com>
2022-07-15 22:34:32 -0400

    Arraylist

--- Engine/src/structs/ArrayList.h
+++ Engine/src/structs/ArrayList.h
@@ -13,17 +13,64 @@ namespace structs {
 
 		}
 
-		ArrayList(int fixedSize) {
+		ArrayList(int fixedSize) :
+		fixedSize(fixedSize) {
 
 		}
 
 		~ArrayList() {
+			clear();
+		}
 
+		void clear() {
+
+		}
+
+		void remove(int index) {
+
+		}
+
+		Object* get(int index) {
+			return nullptr;
+		}
+
+		void insert(int index, const Object& obj) {
+
+		}
+
+		void add(const Object& obj) {
+			if (currentList == nullptr)
+				currentList = new Object[fixedSize];
+			if (pointer >= fixedSize) {
+				temporalList = currentList;
+				currentList = new Object[fixedSize * 2];
+			}
+		}
+
+		void sort() {
+
+		}
+
+		int getSize() {
+			return 0;
+		}
+
+		int getLength() {
+			return 0;
+		}
+
+		bool isEmpty() {
+			return false;
+		}
+
+		Object* toArray() {
+			return nullptr;
 		}
 
 	private:
 
-		int fixedSize;
+		int pointer;
+		int fixedSize = 10;
 
 		Object* currentList;
 		Object* temporalList;
