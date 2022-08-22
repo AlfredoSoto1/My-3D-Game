498627c70795a6ff960cfeb45363e231a4dcb291
AlfredoSoto1 <108200951+AlfredoSoto1@users.noreply.github.com>
2022-08-21 22:53:38 -0400

    buffers

---
+++ Engine/src/buffers/vertexBuffer.cpp
@@ -0,0 +1,122 @@
+#include "vertexBuffer.h"
+#include "../maths/vectors.h"
+
+using namespace maths;
+using namespace buffer;
+
+//struct buffer::Vertex {
+//	vec3 position;
+//	vec3 normal;
+//	unsigned int index;
+//
+//	bool operator == (const Vertex& vertex) {
+//		bool samePosition = position == vertex.position;
+//		bool sameNormal = normal == vertex.normal;
+//		return samePosition && sameNormal;
+//	}
+//};
+
+VertexBuffer::VertexBuffer(unsigned int resizeStep) 
+	: bufferLength(resizeStep), vertexCounter(0), resizeStep(resizeStep)
+{
+	resize();
+}
+
+VertexBuffer::~VertexBuffer() {
+	clear();
+
+	delete[] attribs;
+}
+
+void VertexBuffer::resize() {
+	if (data == nullptr) {
+		data = new Vertex[bufferLength];
+		return;
+	}
+	Vertex* tempData = new Vertex[bufferLength * 2];
+	for (unsigned int i = 0; i < bufferLength; i++)
+		tempData[i] = data[i];
+	delete[] data;
+	data = tempData;
+	bufferLength *= 2;
+}
+
+bool VertexBuffer::needToResize() {
+	return bufferLength == vertexCounter;
+}
+
+template<class T>
+void VertexBuffer::createAttrib(const T& attrib) {
+	unsigned int sizeOfAttrib = sizeof(attrib);
+
+}
+
+void VertexBuffer::add(const Vertex& vertex) {
+	if (needToResize())
+		resize();
+	//add vertex to list
+	data[vertexCounter++] = vertex;
+	//add vertex to list after verifying that its unique
+}
+
+void VertexBuffer::remove(unsigned int location) {
+	for (int i = location; i < vertexCounter - 1; i++)
+		data[i] = data[i + 1];
+	vertexCounter--;
+}
+
+void VertexBuffer::remove(const Vertex& vertex) {
+	int location = find(vertex);
+	if (location != -1)
+		remove((unsigned int)location);
+}
+
+void VertexBuffer::replace(unsigned int location, const Vertex& newVertex) {
+	data[location] = newVertex;
+}
+
+void VertexBuffer::replace(const Vertex& oldVertex, const Vertex& newVertex) {
+	int location = find(oldVertex);
+	if (location != -1)
+		data[location] = newVertex;
+}
+
+Vertex* VertexBuffer::get(unsigned int location) {
+	return &data[location];
+}
+
+bool VertexBuffer::isEmpty() const {
+	return vertexCounter == 0;
+}
+
+bool VertexBuffer::contains(const Vertex& vertex) {
+	if (find(vertex) != -1)
+		return true;
+	else
+		return false;
+}
+
+unsigned int VertexBuffer::getCount() const {
+	return vertexCounter;
+}
+
+int VertexBuffer::find(const Vertex& vertex) {
+	for (unsigned int i = 0; i < vertexCounter; i++) {
+		Vertex& iteratingVertex = data[i];
+		if (iteratingVertex == vertex)
+			return i;
+	}
+	return -1;
+}
+
+void VertexBuffer::compress() {
+
+}
+
+void VertexBuffer::clear() {
+	vertexCounter = 0;
+	bufferLength = resizeStep;
+	if (data != nullptr)
+		delete[] data;
+	data = nullptr;
+}
---
+++ Engine/src/buffers/vertexBuffer.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#ifndef _VERTEX_BUFFER_DEFINED
+#define _VERTEX_BUFFER_DEFINED
+
+#include "../maths/vectors.h"
+
+namespace buffer {
+
+	struct Vertex {
+		maths::vec3 position;
+		maths::vec3 normal;
+		unsigned int index;
+
+		bool operator == (const Vertex& vertex) {
+			bool samePosition = position == vertex.position;
+			bool sameNormal = normal == vertex.normal;
+			return samePosition && sameNormal;
+		}
+	};
+
+	class VertexBuffer {
+	public:
+		VertexBuffer(unsigned int resizeStep);
+		~VertexBuffer();
+
+		template<class T>
+		void createAttrib(const T& attrib);
+
+		void add(const Vertex& vertex);
+		void remove(unsigned int location);
+		void remove(const Vertex& vertex);
+		void replace(unsigned int location, const Vertex& newVertex);
+		void replace(const Vertex& oldVertex, const Vertex& newVertex);
+
+		//temp
+		Vertex* get(unsigned int location);
+
+		bool isEmpty() const;
+		bool contains(const Vertex& vertex);
+
+		unsigned int getCount() const;
+
+		void compress();
+		void clear();
+
+	private:
+		unsigned int vertexCounter;
+		unsigned int bufferLength;
+		unsigned int resizeStep;
+
+		void* attribs;
+
+		Vertex* data;
+
+		int find(const Vertex& vertex);
+
+		void resize();
+		bool needToResize();
+	};
+
+}
+
+#endif // !_VERTEX_BUFFER_DEFINED
